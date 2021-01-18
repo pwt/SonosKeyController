4717c8dbe29810cea841d99d82c1d26bef42be21
Peter Toft <1089749+pwt@users.noreply.github.com>
2021-01-18 17:50:48 +0000

    Tidy up main script

--- sonoskeycontroller.py
+++ sonoskeycontroller.py
@@ -74,67 +74,65 @@ def play_favourite(speaker, favourite):
 
 
 def print_queue(speaker):
-    command = "sonos " + speaker.ip_address + " queue"
-    print(subprocess.getoutput(command))
+    soco_cli_command = "sonos " + speaker.ip_address + " queue"
+    print(subprocess.getoutput(soco_cli_command))
 
 
-# Catch CTL-C
-signal.signal(signal.SIGINT, sigint_handler)
+if __name__ == "__main__":
+    # Catch CTL-C
+    signal.signal(signal.SIGINT, sigint_handler)
 
-
-# Create the SoCo speaker object
-if speaker_ip:
-    speaker = soco.SoCo(speaker_ip)
-else:
-    speaker = soco.discovery.by_name(room_name)
-
-if not speaker:
-    print("Speaker not found ... exiting")
-    exit(0)
-else:
-    print("Sending commands to speaker '{}'".format(speaker.player_name))
-
-print("Waiting for input ... use CTL-C to quit")
-
-# Input loop
-while True:
-    code = wait_for_keypress()
-    timestamp = str(datetime.datetime.now())[:-7]
-    if code in commands:
-        try:
-            command = commands[code]
-            action = command[1]
-            if action == "play":
-                speaker.play()
-                msg = command[0]
-            elif action in ["pause", "stop", "off"]:
-                speaker.pause()
-                msg = command[0]
-            elif action == "next":
-                speaker.next()
-                msg = command[0]
-            elif action == "previous":
-                speaker.previous()
-                msg = command[0]
-            elif action == "queue":
-                print_queue(speaker)
-                msg = command[0]
-            elif action == "relative_volume":
-                vol_change = int(command[2])
-                speaker.set_relative_volume(vol_change)
-                msg = command[0] + " " + str(vol_change)
-            elif action == "favourite":
-                fav = command[2]
-                if play_favourite(speaker, fav):
-                    msg = command[0] + ": " + fav
-                else:
-                    msg = command[0] + ": Could not play " + fav
-            print("{}: {}".format(timestamp, msg))
-        except Exception as error:
-            # Catch any other exceptions to keep running
-            print("{}: Exception caught: {}".format(timestamp, error))
-            pass
+    # Create the SoCo speaker object
+    if speaker_ip:
+        speaker = soco.SoCo(speaker_ip)
     else:
-        print(timestamp + " : key = " + code + ": Unmapped ASCII = " + str(ord(code)))
+        speaker = soco.discovery.by_name(room_name)
 
-# End of Script
+    if not speaker:
+        print("Speaker not found ... exiting")
+        exit(0)
+    else:
+        print("Sending commands to speaker '{}'".format(speaker.player_name))
+
+    print("Waiting for input ... use CTL-C to quit")
+
+    # Input loop
+    while True:
+        code = wait_for_keypress()
+        timestamp = str(datetime.datetime.now())[:-7]
+        if code in commands:
+            try:
+                command = commands[code]
+                action = command[1]
+                if action == "play":
+                    speaker.play()
+                    msg = command[0]
+                elif action in ["pause", "stop", "off"]:
+                    speaker.pause()
+                    msg = command[0]
+                elif action == "next":
+                    speaker.next()
+                    msg = command[0]
+                elif action == "previous":
+                    speaker.previous()
+                    msg = command[0]
+                elif action == "queue":
+                    print_queue(speaker)
+                    msg = command[0]
+                elif action == "relative_volume":
+                    vol_change = int(command[2])
+                    speaker.set_relative_volume(vol_change)
+                    msg = command[0] + " " + str(vol_change)
+                elif action == "favourite":
+                    fav = command[2]
+                    if play_favourite(speaker, fav):
+                        msg = command[0] + ": " + fav
+                    else:
+                        msg = command[0] + ": Could not play " + fav
+                print("{}: {}".format(timestamp, msg))
+            except Exception as error:
+                # Catch any other exceptions to keep running
+                print("{}: Exception caught: {}".format(timestamp, error))
+                pass
+        else:
+            print(timestamp + " : key = " + code + ": Unmapped ASCII = " + str(ord(code)))
